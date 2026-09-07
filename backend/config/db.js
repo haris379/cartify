97917766b663cafee4735507429d16c277ae1a0c
Muhammad Haris <sheikharis574@gmail.com>
2026-09-07 16:48:35 +0500

    Fix

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,11 +5,11 @@ const connectDB = async () => {
 
   try {
     await mongoose.connect(process.env.MONGO_URL);
-    console.log("MongoDB connected Successfully");
+    console.log("MongoDB connected successfully");
   } catch (error) {
-    console.log(`Error : ${error.message}`);
+    console.error("MongoDB connection error:", error.message);
+    process.exit(1);
   }
 };
-
 
 export default connectDB;
